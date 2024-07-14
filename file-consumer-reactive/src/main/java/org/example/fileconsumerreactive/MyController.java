94f94bff3c7181d084c5a9e9564ef21e4cabea9f
Erkenbend <ad.game-adress@hotmail.fr>
2024-07-14 20:26:13 +0200

    implement file download in reactive consumer

--- file-consumer-reactive/src/main/java/org/example/fileconsumerreactive/MyController.java
+++ file-consumer-reactive/src/main/java/org/example/fileconsumerreactive/MyController.java
@@ -1,21 +1,28 @@
 package org.example.fileconsumerreactive;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.example.api.FileApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
 public class MyController {
 
     private final FileApi fileApi;
 
-    @GetMapping("/trigger")
-    public Mono<ResponseEntity<Integer>> triggerFileDownload() {
-        return fileApi.getFile(1, 1)
-                .map(file -> ResponseEntity.ok(file.length));
+    @GetMapping("/trigger/{groupId}")
+    public Mono<ResponseEntity<Integer>> triggerFileDownload(@PathVariable Integer groupId) {
+        return fileApi.getFilesInfo(groupId)
+                .doOnNext(fileInfo -> log.info("Downloading file {}-{} ({} bytes)", groupId, fileInfo.getId(), fileInfo.getContentLength()))
+                .flatMap(fileInfo -> fileApi.getFile(groupId, fileInfo.getId()))
+                .map(fileContent -> fileContent.length)
+                .reduce(Integer::sum)
+                .map(ResponseEntity::ok);
     }
 }
