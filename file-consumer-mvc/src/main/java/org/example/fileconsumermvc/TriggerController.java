f885075b1c6d13462a42741d74984b610471c91e
Erkenbend <ad.game-adress@hotmail.fr>
2024-07-24 18:39:30 +0200

    rewrite mvc trigger-controller with classic java streaming

--- file-consumer-mvc/src/main/java/org/example/fileconsumermvc/TriggerController.java
+++ file-consumer-mvc/src/main/java/org/example/fileconsumermvc/TriggerController.java
@@ -2,10 +2,12 @@ package org.example.fileconsumermvc;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.example.fileprovider.api.FileApi;
 import org.example.fileconsumer.api.TriggerApi;
+import org.example.fileprovider.api.FileApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,11 +20,14 @@ public class TriggerController implements TriggerApi {
     @Override
     public ResponseEntity<Long> trigger(Integer groupId) {
         return fileApi.getFilesInfo(groupId)
-                .doOnNext(fileInfo -> log.info("Downloading file {}-{} ({} bytes)", groupId, fileInfo.getId(), fileInfo.getContentLength()))
-                .flatMap(fileInfo -> fileApi.getFile(groupId, fileInfo.getId()))
+                .toStream()
+                .peek(fileInfo -> log.info("Downloading file {}-{} ({} bytes)", groupId, fileInfo.getId(), fileInfo.getContentLength()))
+                .map(fileInfo -> fileApi.getFile(groupId, fileInfo.getId()).blockOptional())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .map(computeService::computeSize)
                 .reduce(Long::sum)
                 .map(ResponseEntity::ok)
-                .block();
+                .orElseGet(() -> ResponseEntity.noContent().build());
     }
 }
